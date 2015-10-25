fca8d930193e3406d70f22664645ff57b44f3c58
Christian Peters <ctpeters@web.de>
2015-10-25 21:11:55 +0100

    Solution to Programming Assignment 2 for R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function makeCacheMatrix() allows to create a matrix pseudo-object that
+## can cache the matrix itself as well as its inverse.
+## The second function, cacheSolve(), returns the matrix inverse, either
+## from the cache or from calculation.
 
-## Write a short comment describing this function
+# The function makeCacheMatrix creates a list of functions to
+# - cache a matrix                      getMatrix()
+# - retrieve the cached matrix          getMatrix()
+# - cache the inverse of the matrix     setStoredInverse()
+# - retrieve the cached inverse         getStoredInverse()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        storedInverse <- NULL
+        setMatrix <- function(mat){
+                x <<- mat
+                storedInverse <<- NULL
+        }
+        getMatrix <- function() x
+        setStoredInverse <- function(matrixInverse) storedInverse <<- matrixInverse
+        getStoredInverse <- function() storedInverse
+        list(setMatrix = setMatrix, getMatrix = getMatrix,
+             setStoredInverse = setStoredInverse,
+             getStoredInverse = getStoredInverse)
 }
 
 
-## Write a short comment describing this function
+# The function cacheSolve looks at the cached matrix created with
+# makeCacheMatrix(), and calculates and caches its inverse, unless it
+# is alreday cached (ie, getStoredInverse does not return NULL).
+#
+# The function then returns the matrix inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        matrixInverse <- x$getStoredInverse()
+        if(!is.null(matrixInverse)){
+                message("getting chached matrix inverse")
+                return(matrixInverse)
+        }
+        mat <- x$getMatrix()
+        matrixInverse <- solve(mat)
+        x$setStoredInverse(matrixInverse)
+        matrixInverse
 }
